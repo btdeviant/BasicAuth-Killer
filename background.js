8f8511cc6196fd0966f999c05460c964f117fd12
Brandon Williams <Brandon@sweetyhigh.com>
2012-05-30 20:45:17 -0700

    first commit

---
+++ background.js
@@ -0,0 +1,19 @@
+var gPendingCallbacks = [];
+var bkg = chrome.extension.getBackgroundPage();
+
+bkg.console.log("Listening");
+chrome.webRequest.onAuthRequired.addListener(handleAuthRequest,
+  {urls: ["<all_urls>"]}, ["asyncBlocking"]);
+
+function processPendingCallbacks() {
+
+  bkg.console.log("Calling back with credentials");
+  var callback = gPendingCallbacks.pop();
+  callback({authCredentials: {username: "staging",
+                              password: "staging2012sh"}});
+}
+
+function handleAuthRequest(details, callback) {
+  gPendingCallbacks.push(callback);
+  processPendingCallbacks();
+}
